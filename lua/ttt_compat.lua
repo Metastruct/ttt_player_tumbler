c0c8cd45ce39a8ddf3bab6dec07fc9a03118ee88
Ryan <ryan.rbsr@gmail.com>
2024-11-17 12:20:52 +0100

    hopefully this works

--- lua/ttt_compat.lua
+++ lua/ttt_compat.lua
@@ -1,6 +1,6 @@
 if SERVER then
-	local disabled_convars = { "rm_show_tips", "rm_enable_manual", "rm_doorbreaching" }
-	local enabled_convars = { "rm_normal_death_ragdolls" }
+	local disabled_convars = {"rm_show_tips", "rm_enable_manual", "rm_doorbreaching"}
+	local enabled_convars = {"rm_normal_death_ragdolls"}
 
 	for _, cvar in pairs(disabled_convars) do
 		local convar = GetConVar(cvar)
@@ -15,41 +15,32 @@ if SERVER then
 			convar:SetBool(true)
 		end
 	end
+
+	hook.Add("TTT2PostPlayerDeath", "TTT2RagmodRemoveRagdoll", function(ply)
+		if not ragmod then return end
+		local ragdoll = ragmod:GetRagmodRagdoll(ply)
+
+		SafeRemoveEntity(ragdoll)
+	end)
 end
 
 if CLIENT then
 	hook.Add("TTTRenderEntityInfo", "TTT2RagdollTargetID", function(tData)
+		if not targetid then return end
+		if not ragmod then return end
+
 		local ent = tData:GetEntity()
+		if not IsValid(ent) then return end
 
 		-- Check if entity is a ragdoll
-		if not IsValid(ent) or not ent:IsRagdoll() then return end
-		if not ragmod then return end
+		if not ent:IsRagdoll() then return end
 		if not ragmod:IsRagmodRagdoll(ent) then return end
 
 		-- Get the ragdoll owner
 		local owner = ent.GetOwningPlayer and ent:GetOwningPlayer() or nil
 		if not IsValid(owner) then return end
+		if not owner:IsTerror() then return end
 
-		-- Add the player info to the target ID
-		local roleData = roles.GetByIndex(owner:GetRole())
-		if not roleData then return end
-
-		tData:AddIcon(
-			roleData.icon,
-			roleData.color
-		)
-
-		tData:SetKey("name", owner:Nick())
-
-		local subRoleData = roles.GetByIndex(owner:GetSubRole())
-		if not subRoleData then return end
-
-		tData:SetSubtitle(subRoleData.name)
-		tData:AddIcon(
-			subRoleData.icon,
-			subRoleData.color
-		)
-
-		-- You can add more info here as needed
+		targetid.HUDDrawTargetIDPlayers(owner)
 	end)
 end
