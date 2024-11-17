97547019d629ae396db1ba16f4f5f0dffda6c920
Ryan <ryan.rbsr@gmail.com>
2024-11-17 14:57:24 +0100

    this is awful but it should work

--- lua/ttt_compat.lua
+++ lua/ttt_compat.lua
@@ -29,6 +29,41 @@ if SERVER then
 		net.WriteUInt(rag:EntIndex(), 13)
 		net.Broadcast()
 	end)
+
+	hook.Add("TTTCanOrderEquipment", "TTT2RagmodOrderEquipment", function(ply)
+		if ragmod and ragmod:IsRagmodRagdoll(ply) then
+			return false
+		end
+	end)
+
+	hook.Add("RM_RagdollPossessed", "TTT2RagmodRestoreCredits", function(rag, ply)
+		if not IsValid(ply) then return end
+		if ply.IsTerroa and not ply:IsTerror() then return end
+		if GetRoundState and GetRoundState() ~= ROUND_ACTIVE then return end
+
+		ply.next_reset_is_override = true
+	end)
+
+	hook.Add("TTTEndRound", "TTT2RagmodOverrides", function()
+		for _, ply in ipairs(player.GetAll()) do
+			ply.next_reset_is_override = nil
+		end
+	end)
+
+	-- This is gross but it works
+	hook.Add("Initialize", "TTT2RagmodOverrides", function()
+		local PLY = FindMetaTable("Player")
+		PLY.old_ResetRoundFlags = PLY.old_ResetRoundFlags or PLY.ResetRoundFlags
+		PLY.ResetRoundFlags = function(ply) -- This is called on PlayerSpawn, which is called when a player gets up from a ragdoll
+			if ply.next_reset_is_override then
+				ply.next_reset_is_override = nil
+
+				if ply:IsTerror() then return end -- only override for players that are still in the round
+			end
+
+			ply:old_ResetRoundFlags()
+		end
+	end)
 end
 
 if CLIENT then
@@ -44,16 +79,11 @@ if CLIENT then
 		return owner
 	end)
 
-	local remove_rm_menu_hooks = { "Initialize", "InitPostEntity", "HUDPaint" }
-	for _, hook_type in pairs(remove_rm_menu_hooks) do
-		hook.Add(hook_type, "TTT2RemoveRagmodOptions", function()
-			concommand.Remove("rm_menu")
+	hook.Add("Initialize", "TTT2RemoveRagmodOptions", function()
+		concommand.Remove("rm_menu")
 
-			local PLY = FindMetaTable("Player")
-			PLY.RM_OpenMenu = function() end
-			PLY.RM_CloseMenu = function() end
-
-			hook.Remove(hook_type, "TTT2RemoveRagmodOptions")
-		end)
-	end
+		local PLY = FindMetaTable("Player")
+		PLY.RM_OpenMenu = function() end
+		PLY.RM_CloseMenu = function() end
+	end)
 end
